cb74f50c4124fa2e05ab8ca578046c62d316af69
yousefmomar <yousefmomar155@gmail.com>
2025-03-20 05:29:24 +0200

    Update redlock_simulation.py

--- redlock_simulation.py
+++ redlock_simulation.py
@@ -2,8 +2,14 @@ import redis
 import time
 import uuid
 import multiprocessing
+import random
 
 client_processes_waiting = [0, 1, 1, 1, 4]
+
+DEFAULT_RETRY_TIMES = 3
+DEFAULT_RETRY_DELAY = 200
+DEFAULT_TTL = 100000
+CLOCK_DRIFT_FACTOR = 0.01
 
 class Redlock:
     def __init__(self, redis_nodes):
@@ -11,7 +17,11 @@ class Redlock:
         Initialize Redlock with a list of Redis node addresses.
         :param redis_nodes: List of (host, port) tuples.
         """
-        pass
+        self.redis_nodes=redis_nodes
+        self.resource=None
+        self.__lock_id=None
+        self.__ttl=None
+        self.quorum = len(self.redis_nodes) // 2 + 1
 
     def acquire_lock(self, resource, ttl):
         """
@@ -20,7 +30,34 @@ class Redlock:
         :param ttl: Time-to-live for the lock in milliseconds.
         :return: Tuple (lock_acquired, lock_id).
         """
-        pass
+        # lock_key should be random and unique
+        self.__lock_id = uuid.uuid4().hex
+
+        for retry in range(self.retry_times + 1):
+            acquired_node_count = 0
+            start_time = time.monotonic()
+
+            # acquire the lock in all the redis instances sequentially
+            for node in self.redis_nodes:
+                if self.acquire_node(node):
+                    acquired_node_count += 1
+
+            end_time = time.monotonic()
+            elapsed_milliseconds = (end_time - start_time) * 10**3
+
+            # Add 2 milliseconds to the drift to account for Redis expires
+            # precision, which is 1 milliescond, plus 1 millisecond min drift
+            # for small TTLs.
+            drift = (self.__ttl * CLOCK_DRIFT_FACTOR) + 2
+
+            validity = self.__ttl - (elapsed_milliseconds + drift)
+            if acquired_node_count >= self.quorum and validity > 0:
+                return True, validity
+            else:
+                for node in self.redis_nodes:
+                    self.release_node(node)
+                time.sleep(random.randint(0, self.retry_delay) / 1000)
+                
         return False, None
 
     def release_lock(self, resource, lock_id):
@@ -30,6 +67,25 @@ class Redlock:
         :param lock_id: The unique lock ID to verify ownership.
         """
         pass
+    
+    def acquire_node(self, node):
+        """
+        acquire a single redis node
+        """
+        try:
+            return node.set(self.resource, self.lock_key, nx=True, px=self.__ttl)
+        except (redis.exceptions.ConnectionError, redis.exceptions.TimeoutError):
+            return False
+
+    def release_node(self, node):
+        """
+        release a single redis node
+        """
+        # use the lua script to release the lock in a safe way
+        try:
+            node._release_script(keys=[self.resource], args=[self.lock_key])
+        except (redis.exceptions.ConnectionError, redis.exceptions.TimeoutError):
+            pass
 
 def client_process(redis_nodes, resource, ttl, client_id):
     """
